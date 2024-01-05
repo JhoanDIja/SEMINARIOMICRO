a3771b8343d0cd589f297a5019ab2ab1718f5e72
Jhoan DIja <albertdija@gmail.com>
2024-01-05 15:35:00 -0400

    Funcion numero 3 en proceso

--- PRUEBAS.py
+++ PRUEBAS.py
@@ -21,11 +21,29 @@ def LeerArchivo(archivo):
     except:
         print("Este archivo no existe")
 
-# LeerArchivo("/home/albertdipre/Desktop/SEMINARIOMICRO/PRUEBASPY/miarchsivo.txt")
+LeerArchivo("/home/albertdipre/Desktop/SEMINARIOMICRO/PRUEBASPY/miarchsivo.txt")
 
 """
 Una función para escribir un texto que le pasen como argumento, en un archivo cualquiera, y
 devuelve lo que escribió.
 """
+def EscribirArchivo(ArchivoEscrito,texto):
+    try:
+        textfile=open(ArchivoEscrito,"x")
+        textfile.write(texto)
+        return textfile
+    except:
+        print("No se puede escribir en este archivo o intentaste poner un archivo ya existente")
+    
+EscribirArchivo("/home/albertdipre/Desktop/SEMINARIOMICRO/PRUEBASPY/miarchsivo.txt")
+
+
+def CambiarPalabra(a):
+    for i in range(0,3):
+        try:
+            textfile =open(a,"x")
+        except:
+            print("Error")
+
 
 
